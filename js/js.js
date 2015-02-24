10ea12ec5c006f1bfa8b52944e098008b4a4fd86
unknown <lydexmail@yahoo.com>
2015-02-24 22:05:02 +0100

    Added pre-loader

--- js/js.js
+++ js/js.js
@@ -9,7 +9,6 @@ var gitSearch = {
 
     getEntry: function() {
     $('#searchButton').click(function() {
-      console.log("hello");
       $('#repo_list ul').empty();
       var query = $('#textbox').val();
       if( query != "") {
@@ -18,7 +17,6 @@ var gitSearch = {
         $('#loader').show();        
         gitSearch.getuserInfo(query);        
         gitSearch.getRepos(query);
-        console.log(query);
       }
       else {
         $('#loader, #image, #info, #repo_list').hide();
@@ -32,7 +30,6 @@ var gitSearch = {
       
     $.getJSON(gitSearch.gitbase + "users/" + username + "?client_id=" + gitSearch.clientID + "&client_secret=" + gitSearch.clientSecret, function(response) {
       $('#textbox').val("");
-      console.log(response);
       var organization = gitSearch.getOrganisations(username);
       var name = response.name;
       var avatar = response.avatar_url;
@@ -40,12 +37,10 @@ var gitSearch = {
       var following = response.following;
       var numOfRepos = response.public_repos;
       var org = response.company
-      //console.log(org);
       $('#loader').hide();
       $('#image').show();
       $('#info').show();
-      //$('#image').html("<img src=" + avatar + "/>");
-      $('img').attr('src',avatar).addClass("img-responsive").addClass("img-rounded");
+      $('#info-box li img').attr('src',avatar).addClass("img-responsive").addClass("img-rounded");
       $('#name').html("  Name: " + name);
       $('#repo span').html("  Public Repositories: ");
       $('#followers span').html(followers);
@@ -69,23 +64,12 @@ var gitSearch = {
         $('#error').html("<p>Could not process request, are you connected to the internet?</p>").show();
       }
     });
-    gitSearch.styleResult();
-  },
-  styleResult: function() {
-    //console.log("hjsdghhjsagdsadsd")
-    $("#name").addClass("fa fa-user");
-    $('#pub_repo').addClass("fa fa-folder-open");
-    $('#follower').addClass("fa fa-user-plus");
-    $('#following').addClass("fa fa-user-plus");
-    $('#repo_num').addClass("fa fa-briefcase");
-    $('#org_num').addClass("fa fa-university");
   },
   getRepos: function(user) {
     $.getJSON(gitSearch.gitbase + "users/" + user + "/repos?client_id=" + gitSearch.clientID + "&client_secret=" + gitSearch.clientSecret, function(data) {
       $.each(data, function(index,value) {
         var repolist = data[index].clone_url;
         var repoName = data[index].full_name;
-        //$('#repo_list').show();
         $('#repo_list ul').append("<li><a href=" + repolist + " target=blank>" + repoName + "</a></li><br>");
         $('li #pub_repos').click(function() {
           $('#repo_list').show();
@@ -96,10 +80,10 @@ var gitSearch = {
 
   getOrganisations: function(userQuery) {
     var org_num = 0;
-    $.getJSON(gitSearch.gitbase + "users/" + userQuery +" /orgs?client_id=" + gitSearch.clientID + "&client_secret=" + gitSearch.clientSecret, function(resp) {
-    org_num = resp.length;     
+    $.getJSON(gitSearch.gitbase + "users/" + userQuery +"/orgs?client_id=" + gitSearch.clientID + "&client_secret=" + gitSearch.clientSecret, function(resp) {
+    org_num = resp.length;
+    $('#orgz span').html(org_num);    
     });
-    return org_num;
   }
 };
 
